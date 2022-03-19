e472ea2f425b1fda3f089e74c7be5cdbc367c7c1
Tom Werth <erfinder.tom@gmail.com>
2022-03-19 22:22:07 +0100

    added option to set java Falgs to the Config & aikars Flags

--- src/main/java/de/tomino/serverjars/Config.kt
+++ src/main/java/de/tomino/serverjars/Config.kt
@@ -1,16 +1,17 @@
-package de.tomino.serverjars
+package de.tomino.serverjars;
 
 import java.io.File
-import java.util.Properties
-import java.io.IOException
 import java.io.FileInputStream
-import java.nio.file.Files
 import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.file.Files
+import java.util.*
 
 class Config(private val file: File) {
-    private val properties: Properties = Properties()
+    private val properties: Properties
 
     init {
+        properties = Properties()
         reset()
     }
 
@@ -23,6 +24,43 @@ class Config(private val file: File) {
         get() = properties.getProperty("version", DEFAULT_VERSION)
         set(version) {
             properties.setProperty("version", version ?: DEFAULT_VERSION)
+        }
+
+    fun setJvmArgs(jvmArgs: String?) {
+        properties.setProperty("jvmArgs", jvmArgs)
+    }
+
+    val jvmArgs: Array<String>
+        get() {
+            val args = properties.getProperty("jvmArgs", DEFAULT_JVM_ARGS).split(" -").toTypedArray()
+            for (i in args.indices) {
+                args[i] = (if (i != 0) "-" else "") + args[i].trim { it <= ' ' }
+            }
+            return args
+        }
+    val serverJarDirectory: String
+        get() = properties.getProperty("MinecraftJarDirectory", DEFAULT_SERVER_JAR_DIRECTORY)
+
+    fun shouldDeleteOtherJarVersions(): Boolean {
+        return java.lang.Boolean.parseBoolean(properties.getProperty("deleteOtherMinecraftJarVersions", "true"))
+    }
+
+    fun setLastUpdateCheck(timestamp: Long) {
+        properties.setProperty("updateCheck.last", timestamp.toString())
+    }
+
+    val lastUpdateCheck: Long?
+        get() {
+            try {
+                return properties.getProperty("updateCheck.last").toLong()
+            } catch (ignore: NumberFormatException) {
+            }
+            return null
+        }
+    var isUpdateAvailable: Boolean
+        get() = java.lang.Boolean.parseBoolean(properties.getProperty("updateCheck.available"))
+        set(isAvailable) {
+            properties.setProperty("updateCheck.available", isAvailable.toString())
         }
 
     @Throws(IOException::class)
@@ -41,15 +79,20 @@ class Config(private val file: File) {
         FileOutputStream(file).use { out -> properties.store(out, HEADER) }
     }
 
-    private fun reset() {
+    fun reset() {
         properties.clear()
-        properties.setProperty("type", DEFAULT_TYPE)
-        properties.setProperty("version", DEFAULT_VERSION)
+        type = DEFAULT_TYPE
+        version = DEFAULT_VERSION
+        setJvmArgs(DEFAULT_JVM_ARGS)
+        properties.setProperty("MinecraftJarDirectory", DEFAULT_SERVER_JAR_DIRECTORY)
+        properties.setProperty("deleteOtherMinecraftJarVersions", "true")
     }
 
     companion object {
         private const val HEADER = "Acceptable Versions (latest, 1.16.4, 1.8, etc...)"
         private const val DEFAULT_TYPE = "paper"
         private const val DEFAULT_VERSION = "latest"
+        private const val DEFAULT_JVM_ARGS = "-Xmx1G"
+        private const val DEFAULT_SERVER_JAR_DIRECTORY = "./jar"
     }
 }
--- src/main/java/de/tomino/serverjars/ServerJars.kt
+++ src/main/java/de/tomino/serverjars/ServerJars.kt
@@ -30,6 +30,7 @@ object ServerJars {
 
         println("\n  █████╗  █████╗ ███████╗ █████╗ ███╗  ██╗ ██████╗██████╗ ██╗██████╗ ███████╗\n ██╔══██╗██╔══██╗██╔════╝██╔══██╗████╗ ██║██╔════╝██╔══██╗██║██╔══██╗██╔════╝\n ██║  ██║██║  ╚═╝█████╗  ███████║██╔██╗██║╚█████╗ ██████╔╝██║██████╔╝█████╗\n ██║  ██║██║  ██╗██╔══╝  ██╔══██║██║╚████║ ╚═══██╗██╔═══╝ ██║██╔══██╗██╔══╝\n ╚█████╔╝╚█████╔╝███████╗██║  ██║██║ ╚███║██████╔╝██║     ██║██║  ██║███████╗\n  ╚════╝  ╚════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚══╝╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝")
         println("\nSearching for updates...")
+
         var jar: File?
 
         jar = if (!CFG_FILE.exists()) {
@@ -101,6 +102,17 @@ object ServerJars {
                 for (typeList in typeMap.values) {
                     types.addAll(typeList)
                 }
+
+                cfg.setJvmArgs("-Xms" + {} + " -Xmx" + {} + " -XX:+UseG1GC -XX:+ParallelRefProcEnabled " +
+                        "-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC " +
+                        "-XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 " +
+                        "-XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 " +
+                        "-XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 " +
+                        "-XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 " +
+                        "-XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 " +
+                        "-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true");
+
+
                 println("""What server type would you like to use? Available types:""".trimIndent())
                 val typeString = StringBuilder()
                 var i = 0
@@ -189,7 +201,7 @@ object ServerJars {
         return files?.get(0)
     }
 
-    private fun awaitInput(predicate: Predicate<String>, errorMessage: String): String? {
+    fun awaitInput(predicate: Predicate<String>, errorMessage: String): String? {
         try {
             val bufferedReader = BufferedReader(InputStreamReader(System.`in`))
             var line: String
